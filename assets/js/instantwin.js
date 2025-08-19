e71c0a994978aa9baa7447e1055f77a226feb218
Duy N <duyn@Duys-iMac.local>
2025-08-19 09:29:42 +0700

    Update wheel CSS styling - change border color, remove double border, update center knob size to 25px

--- assets/js/instantwin.js
+++ assets/js/instantwin.js
@@ -719,7 +719,7 @@ jQuery(document).ready(function($) {
       <div class="wheel-container" style="position:relative;max-width:400px;margin:0 auto;">
         <canvas id="${canvasId}" width="400" height="400" aria-label="Prize Wheel" style="display:block;"></canvas>
         <div id="${pointerId}" style="position:absolute;top:12px;left:50%;transform:translateX(-50%);width:0;height:0;border-left:8px solid transparent;border-right:8px solid transparent;border-bottom:16px solid #e74c3c;z-index:2;"></div>
-        <div class="wheel-center-knob" style="position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);width:35px;height:35px;background:white;border-radius:50%;z-index:3;"></div>
+        <div class="wheel-center-knob" style="position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);width:25px;height:25px;background:white;border-radius:50%;z-index:3;"></div>
       </div>
     `);
     
